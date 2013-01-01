bf1feacd2227394bab482ef07be1f9734f32e704
Tycho Andersen <tycho@tycho.ws>
2012-12-31 23:05:20 -0600

    Filled out processing api

--- src/Hquery/Transform.hs
+++ src/Hquery/Transform.hs
@@ -2,18 +2,22 @@
 
 module Hquery.Transform where
 
-import Data.Text
+import qualified Data.Text as T
+import Data.List
 import Text.XmlHtml
 import Text.XmlHtml.Cursor
 
 import Hquery.Selector
 
-buildAttrMod :: AttrSel -> Text -> Cursor -> Cursor
-buildAttrMod (AttrSel name attrMod) value = case attrMod of
-  Set -> do
-    let f = setAttribute name (value)
-    modifyNode f
-  _ -> id
+buildAttrMod :: AttrSel -> T.Text -> Cursor -> Cursor
+buildAttrMod (AttrSel name attrMod) value cur =
+  let f = case attrMod of
+            Set -> setAttribute name (value)
+            Remove -> setAttribute name ""
+            Append -> do
+              let a = maybe "" id (getAttribute name (current cur))
+              setAttribute name (T.append a value)
+  in modifyNode f cur
 
 transform :: CssSel -> (Cursor -> Cursor) -> Node -> Node
 transform sel f rootNode = topNode (transformR (fromNode rootNode))
@@ -27,4 +31,24 @@ transform sel f rootNode = topNode (transformR (fromNode rootNode))
         case getAttribute "id" node of
           Just id_ | id_ == name -> f cur
           _ -> cur
-      _ -> cur
+      Name name -> do
+        let node = current cur
+        case getAttribute "name" node of
+          Just id_ | id_ == name -> f cur
+          _ -> cur
+      Class name -> do
+        let node = current cur
+        case getAttribute "class" node of
+          Just classes | isInfixOf [name] (T.words classes) -> f cur
+          _ -> cur
+      Attr key value -> do
+        let node = current cur
+        case getAttribute key node of
+          Just id_ | id_ == value -> f cur
+          _ -> cur
+      Elem name -> do
+        let node = current cur
+        case tagName node of
+          Just id_ | id_ == name -> f cur
+          _ -> cur
+      Star -> f cur
