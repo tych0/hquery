e102a749cfe78abfc683166d53f5649d63d2329a
Tycho Andersen <tycho@tycho.ws>
2013-01-01 20:40:57 -0600

    Fix a typo, formatting fixes

--- src/Hquery/Transform.hs
+++ src/Hquery/Transform.hs
@@ -22,19 +22,19 @@ transform sel f rootNode = topNode (transformR (fromNode rootNode))
   where
     transformR cur = do
       let result = process cur
-      maybe cur transformR (nextDF result)
+      maybe result transformR (nextDF result)
     process cur = do
-    let node = current cur
-    let matchAttr attr pred_ = case getAttribute attr node of
-                                Just value | pred_ value -> f cur
-                                _ -> cur
-    case sel of
-      Id name -> matchAttr "id" ((==) name)
-      Name name -> matchAttr "name" ((==) name)
-      Class name -> matchAttr "class" (\x -> isInfixOf [name] (T.words x))
-      Attr key value -> matchAttr key ((==) value)
-      Elem name ->
-        case tagName node of
-          Just id_ | id_ == name -> f cur
-          _ -> cur
-      Star -> f cur
+      let node = current cur
+      let matchAttr attr pred_ = case getAttribute attr node of
+                                   Just value | pred_ value -> f cur
+                                   _ -> cur
+      case sel of
+        Id name -> matchAttr "id" ((==) name)
+        Name name -> matchAttr "name" ((==) name)
+        Class name -> matchAttr "class" (\x -> isInfixOf [name] (T.words x))
+        Attr key value -> matchAttr key ((==) value)
+        Elem name ->
+          case tagName node of
+            Just id_ | id_ == name -> f cur
+            _ -> cur
+        Star -> f cur
