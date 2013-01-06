f91ac2fe8731524a9caedc8b1401c0b47a653337
Tycho Andersen <tycho@tycho.ws>
2013-01-06 08:46:13 -0600

    Fix class removal error

--- src/Hquery/Transform.hs
+++ src/Hquery/Transform.hs
@@ -8,14 +8,17 @@ import Text.XmlHtml.Cursor
 import Hquery.Selector
 
 buildAttrMod :: AttrSel -> T.Text -> Cursor -> Cursor
-buildAttrMod (AttrSel name attrMod) value cur =
+buildAttrMod (AttrSel name attrMod) value cur = do
+  let att = maybe "" id (getAttribute name (current cur))
   let f = case attrMod of
             Set -> setAttribute name (value)
+            Remove | name == "class" -> do
+              let classes = T.words value
+              let without = filter ((flip notElem) classes) (T.words att)
+              setAttribute name (T.intercalate "" without)
             Remove -> setAttribute name ""
-            Append -> do
-              let a = maybe "" id (getAttribute name (current cur))
-              setAttribute name (T.append a value)
-  in modifyNode f cur
+            Append -> setAttribute name (T.append att value)
+  modifyNode f cur
 
 transform :: CssSel -> (Cursor -> Cursor) -> Node -> Node
 transform sel f rootNode = topNode (transformR (fromNode rootNode))
